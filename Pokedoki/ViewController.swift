737c247d101fd7cb3ad657fabf37b8496bd438d8
hassen klai <klai@hassens-MacBook-Pro.local>
2016-07-13 15:06:09 -0400

    add music

--- Pokedoki/ViewController.swift
+++ Pokedoki/ViewController.swift
@@ -7,19 +7,49 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController ,UICollectionViewDelegate ,UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
     
+    @IBOutlet weak var musicImg :UIButton!
     @IBOutlet weak var collectionsView : UICollectionView!
     var pokemon = [Pokemon]()
-
+    var musicPlayer : AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionsView.delegate = self
         collectionsView.dataSource = self
+        initAudio()
         parsePokemonCSV()
         
+        musicImg.layer.cornerRadius = musicImg.frame.width/2
+        musicImg.clipsToBounds = true
+    }
+    
+    func initAudio() {
+        let path = NSBundle.mainBundle().pathForResource("PokemonSongFull", ofType: "mp3")
+        do {
+            musicPlayer = try AVAudioPlayer(contentsOfURL: NSURL(string: path!)!)
+            musicPlayer.prepareToPlay()
+            musicPlayer.numberOfLoops = -1
+            musicPlayer.play()
+        }
+        catch let err as NSError {
+            print(err.description)
+        }
+    }
+    
+    @IBAction func musicPressedButton(sender :UIButton!){
+        if musicPlayer.playing {
+            musicPlayer.stop()
+            sender.alpha = 0.5
+        }
+        else {
+            musicPlayer.play()
+            sender.alpha = 1.0
+        }
     }
     
     func parsePokemonCSV(){
@@ -40,33 +70,26 @@ class ViewController: UIViewController ,UICollectionViewDelegate ,UICollectionVi
             print(err.description)
         }
     }
-    
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         if let cell = collectionView.dequeueReusableCellWithReuseIdentifier("pokeCell", forIndexPath: indexPath) as? pokeCell{
-            
             let pokem = pokemon[indexPath.row]
             cell.configureCell(pokem)
-    
             return cell
         }
-            
         else
         {
             return UICollectionViewCell()
         }
     }
-    
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
         
     }
-    
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
         return 1
     }
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return pokemon.count
     }
-    
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
         return CGSizeMake(105, 105)
     }
